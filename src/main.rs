b3d17ae618bd65de28a307ff4bbd4d7f6b99bc2d
Isaac Adams <isaacdadams@pm.me>
2023-10-01 19:58:20 -0400

    adding a main function

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use pokemon_utils::{self, is_rare, PokedexEntry};
+
+fn main() {
+    let pokemon = pokemon_utils::encounter_random_pokemon();
+    let pokemon = PokedexEntry::get_by_id(pokemon as usize).unwrap();
+    println!("found a wild {}!", pokemon.name);
+    println!("is rare? {}", is_rare(&pokemon.index));
+}
